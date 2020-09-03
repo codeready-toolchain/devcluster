cca8a8edc8e7581706e037f52cc722d4e52d1146
Alexey Kazakov <alkazako@redhat.com>
2020-09-03 13:07:09 -0700

    Add secret

--- make/dev.mk
+++ make/dev.mk
@@ -38,18 +38,40 @@ reset-namespace: login-as-admin clean-namespace create-namespace
 
 .PHONY: deploy-on-minishift
 ## Deploy DevCluster service on minishift
-deploy-on-minishift: login-as-admin create-namespace build docker-image-dev
-	$(Q)oc process -f ./deploy/devcluster.yaml \
-        -p IMAGE=${IMAGE_DEV} \
-        -p ENVIRONMENT=dev \
-        -p NAMESPACE=${NAMESPACE} \
-        | oc apply -f -
+deploy-on-minishift: login-as-admin create-namespace build docker-image-dev apply-resources print-route
 
 .PHONY: deploy-dev
 ## Deploy DevCluster on dev environment
-deploy-dev: create-namespace build docker-image-dev docker-push-dev
+deploy-dev: create-namespace build docker-image-dev docker-push-dev apply-resources print-route
+
+.PHONY: apply-resources
+## Apply DevCluster resources
+apply-resources:
 	$(Q)oc process -f ./deploy/devcluster.yaml \
         -p IMAGE=${IMAGE_DEV} \
         -p ENVIRONMENT=dev \
         -p NAMESPACE=${NAMESPACE} \
         | oc apply -f -
+    # Try to create a secret. Will fail if it already exists. So we don't override the existing one.
+	$(Q)-oc process -f ./deploy/secret.yaml \
+        -p NAMESPACE=${NAMESPACE} \
+        | oc create -f -
+
+.PHONY: print-route
+print-route:
+	@echo "------------------------------------------------------------------"
+	@echo "Deployment complete! Waiting for the devcluster service route."
+	@echo -n "."
+	@while [[ -z `oc get routes devcluster -n ${NAMESPACE} 2>/dev/null` ]]; do \
+		if [[ $${NEXT_WAIT_TIME} -eq 100 ]]; then \
+            echo ""; \
+            echo "The timeout of waiting for the service route has been reached. Try to run 'make print-route' later or check the deployment logs"; \
+            exit 1; \
+		fi; \
+		echo -n "."; \
+		sleep 1; \
+	done
+	@echo ""
+	$(eval ROUTE = $(shell oc get routes devcluster -n ${NAMESPACE} -o=jsonpath='{.spec.host}'))
+	@echo Access the Landing Page here: https://${ROUTE}
+	@echo "------------------------------------------------------------------"
