345ae0526743a309becee9f8608c53da53ee4475
Alexey Kazakov <alkazako@redhat.com>
2020-09-03 20:00:32 -0700

    Add clister create

--- pkg/ibmcloud/ibmcloud.go
+++ pkg/ibmcloud/ibmcloud.go
@@ -3,10 +3,12 @@ package ibmcloud
 import (
 	"bytes"
 	"encoding/json"
+	"fmt"
 	"io"
 	"net/http"
 	"net/url"
 	"strings"
+	"sync"
 	"time"
 
 	"github.com/alexeykazakov/devcluster/pkg/rest"
@@ -14,52 +16,149 @@ import (
 	"github.com/pkg/errors"
 )
 
-// Tokens obtains an access token
+type Configuration interface {
+	GetIBMCloudAPIKey() string
+}
+
+type Client struct {
+	config   Configuration
+	token    *TokenSet
+	tokenMux sync.RWMutex
+}
+
+func NewClient(config Configuration) *Client {
+	return &Client{
+		config: config,
+	}
+}
+
+func (c *Client) GetToken() TokenSet {
+	defer c.tokenMux.RUnlock()
+	c.tokenMux.RLock()
+	return *c.token
+}
+
+// Token returns IBM Cloud Token.
+// If the token is expired or not obtained yet it will obtain a new one.
+func (c *Client) Token() (TokenSet, error) {
+	c.tokenMux.RLock()
+	if tokenExpired(c.token) {
+		c.tokenMux.RUnlock()
+		c.tokenMux.Lock()
+		defer c.tokenMux.Unlock()
+		var err error
+		if tokenExpired(c.token) {
+			c.token, err = c.obtainNewToken()
+		}
+		return *c.token, err
+	}
+	defer c.tokenMux.RUnlock()
+	return *c.token, nil
+}
+
+// tokenExpired return false if the token is not nil and good for at least one more minute
+func tokenExpired(token *TokenSet) bool {
+	return token == nil || time.Now().After(time.Unix(token.Expiration-60, 0))
+}
+
+const clusterConfigTemplate = `
+{
+  "dataCenter": "wdc04",
+  "disableAutoUpdate": true,
+  "machineType": "b3c.4x16",
+  "masterVersion": "4.4_openshift",
+  "name": "%s",
+  "publicVlan": "2940148",
+  "privateVlan": "2940150",
+  "workerNum": 2
+}`
+
+// CreateCluster creates a cluster
+// Returns the cluster ID
+func (c *Client) CreateCluster(name string) (string, error) {
+	token, err := c.Token()
+	if err != nil {
+		return "", err
+	}
+	body := bytes.NewBuffer([]byte(fmt.Sprintf(clusterConfigTemplate, name)))
+	req, err := http.NewRequest("POST", "https://containers.cloud.ibm.com/global/v1/clusters", body)
+	if err != nil {
+		return "", err
+	}
+	req.Header.Add("Authorization", "Bearer "+token.AccessToken)
+	res, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return "", errors.Wrap(err, "unable to create cluster")
+	}
+	defer rest.CloseResponse(res)
+	bodyString := rest.ReadBody(res.Body)
+	if res.StatusCode != http.StatusCreated {
+		return "", errors.Errorf("unable to obtain access token from IBM Cloud. Response status: %s. Response body: %s", res.Status, bodyString)
+	}
+
+	var objmap map[string]json.RawMessage
+	err = json.Unmarshal([]byte(bodyString), &objmap)
+	if err != nil {
+		return "", errors.Wrapf(err, "error when unmarshal json with cluster ID %s ", bodyString)
+	}
+
+	var idObj ID
+	if err != nil {
+		return "", errors.Wrapf(err, "error when unmarshal json with cluster ID %s ", bodyString)
+	}
+	return idObj.ID, nil
+}
+
+type ID struct {
+	ID string `json:"id"`
+}
+
+// obtainNewToken obtains an access token
 // Returns the access token string and the time when the token is going to expire
-func Token(apikey string) (string, time.Time, error) {
+func (c *Client) obtainNewToken() (*TokenSet, error) {
 	client := &http.Client{Timeout: 10 * time.Second}
 	res, err := client.PostForm("https://iam.cloud.ibm.com/identity/token", url.Values{
 		"grant_type": {"urn:ibm:params:oauth:grant-type:apikey"},
-		"apikey":     {apikey},
+		"apikey":     {c.config.GetIBMCloudAPIKey()},
 	})
 	if err != nil {
-		return "", time.Time{}, err
+		return nil, err
 	}
 
 	defer rest.CloseResponse(res)
 	if res.StatusCode != http.StatusOK {
 		bodyString := rest.ReadBody(res.Body)
-		return "", time.Time{}, errors.Errorf("unable to obtain access token from IBM Cloud. Response status: %s. Response body: %s", res.Status, bodyString)
+		return nil, errors.Errorf("unable to obtain access token from IBM Cloud. Response status: %s. Response body: %s", res.Status, bodyString)
 	}
-	tokenSet, err := ReadTokenSet(res)
+	tokenSet, err := readTokenSet(res)
 	if err != nil {
-		return "", time.Time{}, err
+		return nil, err
 	}
 	if tokenSet.AccessToken == "" {
-		return "", time.Time{}, errors.New("unable to obtain access token from IBM Cloud. Access Token is missing in the response")
+		return nil, errors.New("unable to obtain access token from IBM Cloud. Access Token is missing in the response")
 	}
-	expiry := time.Now().Add((time.Duration(tokenSet.ExpiresIn) - 60) * time.Second) // Subtract 1 minute from the expiry
-	return tokenSet.AccessToken, expiry, nil
+	return tokenSet, nil
 }
 
 // TokenSet represents a set of Access and Refresh tokens
 type TokenSet struct {
 	AccessToken  string `json:"access_token"`
 	ExpiresIn    int64  `json:"expires_in"`
+	Expiration   int64  `json:"expiration"`
 	RefreshToken string `json:"refresh_token"`
 	TokenType    string `json:"token_type"`
 }
 
-// ReadTokenSet extracts json with token data from the response
-func ReadTokenSet(res *http.Response) (*TokenSet, error) {
+// readTokenSet extracts json with token data from the response
+func readTokenSet(res *http.Response) (*TokenSet, error) {
 	buf := new(bytes.Buffer)
 	io.Copy(buf, res.Body)
 	jsonString := strings.TrimSpace(buf.String())
-	return ReadTokenSetFromJson(jsonString)
+	return readTokenSetFromJson(jsonString)
 }
 
-// ReadTokenSetFromJson parses json with a token set
-func ReadTokenSetFromJson(jsonString string) (*TokenSet, error) {
+// readTokenSetFromJson parses json with a token set
+func readTokenSetFromJson(jsonString string) (*TokenSet, error) {
 	var token TokenSet
 	err := json.Unmarshal([]byte(jsonString), &token)
 	if err != nil {
--- pkg/ibmcloud/ibmcloud_test.go
+++ pkg/ibmcloud/ibmcloud_test.go
@@ -2,7 +2,6 @@ package ibmcloud_test
 
 import (
 	"testing"
-	"time"
 
 	"github.com/alexeykazakov/devcluster/pkg/ibmcloud"
 
@@ -12,7 +11,10 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
+var mockConfig = &MockConfig{}
+
 func TestToken(t *testing.T) {
+	cl := ibmcloud.NewClient(mockConfig)
 	t.Run("OK", func(t *testing.T) {
 		defer gock.OffAll()
 
@@ -22,9 +24,22 @@ func TestToken(t *testing.T) {
 			Reply(200).
 			BodyString(`{"access_token": "abc","refresh_token":"qwerty","ims_user_id": 4778951,"token_type": "Bearer","expires_in": 3600,"expiration": 1598983098,"scope":"ibm openid"}`)
 
-		token, expiry, err := ibmcloud.Token("secretkey")
+		token, err := cl.Token()
 		require.NoError(t, err)
-		assert.Equal(t, "abc", token)
-		assert.True(t, expiry.After(time.Now()) && expiry.Before(time.Now().Add(time.Duration(3600)*time.Second)))
+		require.NotNil(t, token)
+		assert.Equal(t, ibmcloud.TokenSet{
+			AccessToken:  "abc",
+			RefreshToken: "qwerty",
+			ExpiresIn:    3600,
+			Expiration:   1598983098,
+			TokenType:    "Bearer",
+		}, token)
 	})
+}
+
+type MockConfig struct {
+}
+
+func (c *MockConfig) GetIBMCloudAPIKey() string {
+	return "secretkey"
 }
