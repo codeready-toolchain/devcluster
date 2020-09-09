bd658eb1be45adeeb4e0b3ff923439cb346fe54e
Alexey Kazakov <alkazako@redhat.com>
2020-09-08 19:21:09 -0700

    Tests for resume provisioning

--- test/integration_test.go
+++ test/integration_test.go
@@ -1,10 +1,14 @@
 package test
 
 import (
+	"context"
 	"fmt"
 	"strings"
 	"testing"
 	"time"
+
+	"github.com/codeready-toolchain/devcluster/pkg/mongodb"
+	"go.mongodb.org/mongo-driver/bson"
 
 	"github.com/codeready-toolchain/devcluster/pkg/cluster"
 	"github.com/codeready-toolchain/devcluster/pkg/configuration"
@@ -46,36 +50,82 @@ func (s *TestIntegrationSuite) TestRequestService() {
 	request2 := s.newRequest(service, numberOfClustersPerReq)
 
 	s.Run("request is provisioning", func() {
-		err := waitForClustersToStartProvisioning(service, request1)
+		reqWithClusters1, err := waitForClustersToStartProvisioning(service, request1)
 		require.NoError(s.T(), err)
-		err = waitForClustersToStartProvisioning(service, request2)
+		_, err = waitForClustersToStartProvisioning(service, request2)
 		require.NoError(s.T(), err)
 
 		s.Run("provisioned", func() {
 			// Update all clusters as provisioned in the mock client
-			r, err := service.GetRequestWithClusters(request1.ID)
-			require.NoError(s.T(), err)
-			for _, c := range r.Clusters {
-				err := mockClient.UpdateCluster(ibmcloud.Cluster{
-					ID:      c.ID,
-					State:   "normal",
-					Ingress: ibmcloud.Ingress{Hostname: fmt.Sprintf("prefix-%s", c.Name)},
-				})
-				require.NoError(s.T(), err)
-			}
+			s.markClustersAsProvisioned(service, mockClient, request1)
 
 			// Check that the request is now also returned as provisioned
-			err = waitForClustersToGetProvisioned(service, request1)
+			_, err = waitForClustersToGetProvisioned(service, request1)
 			require.NoError(s.T(), err)
+
+			s.Run("resume provisioning", func() {
+				// Delete some clusters from mongo to imitate the case when provisioning was interrupted (i.g. if pod was killed)
+				// And set others in deploying state
+				_, err := mongodb.Clusters().DeleteOne(
+					context.Background(),
+					bson.D{
+						{"_id", reqWithClusters1.Clusters[0].ID},
+					},
+				)
+				require.NoError(s.T(), err)
+				_, err = mongodb.Clusters().UpdateOne(
+					context.Background(),
+					bson.D{
+						{"_id", reqWithClusters1.Clusters[1].ID},
+					},
+					bson.D{
+						{"$set", bson.D{
+							{"status", "deploying"},
+							{"url", ""},
+						}},
+					},
+				)
+				require.NoError(s.T(), err)
+
+				// Now resume provisioning
+				err = service.ResumeProvisioningRequests()
+				require.NoError(s.T(), err)
+
+				// Verify that all clusters are now provisioning
+				_, err = waitForClustersToStartProvisioning(service, request2)
+				require.NoError(s.T(), err)
+
+				// Update all clusters as provisioned in the mock client
+				s.markClustersAsProvisioned(service, mockClient, request2)
+
+				// Verify that all clusters are now provisioned
+				_, err = waitForClustersToGetProvisioned(service, request2)
+				require.NoError(s.T(), err)
+			})
 		})
 	})
+}
+
+func (s *TestIntegrationSuite) markClustersAsProvisioned(service *cluster.ClusterService, client *MockIBMCloudClient, request cluster.Request) {
+	// Update all clusters as provisioned in the mock client
+	r, err := service.GetRequestWithClusters(request.ID)
+	require.NoError(s.T(), err)
+	for _, c := range r.Clusters {
+		err := client.UpdateCluster(ibmcloud.Cluster{
+			ID:      c.ID,
+			State:   "normal",
+			Ingress: ibmcloud.Ingress{Hostname: fmt.Sprintf("prefix-%s", c.Name)},
+		})
+		require.NoError(s.T(), err)
+	}
 }
 
 var retryInterval = 100 * time.Millisecond
 var timeout = 5 * time.Second
 var numberOfClustersPerReq = 10
 
-func waitForClustersToStartProvisioning(service *cluster.ClusterService, request cluster.Request) error {
+func waitForClustersToStartProvisioning(service *cluster.ClusterService, request cluster.Request) (cluster.RequestWithClusters, error) {
+	var req cluster.RequestWithClusters
 	err := wait.Poll(retryInterval, timeout, func() (done bool, err error) {
 		fmt.Println("Wait for clusters to start provisioning")
 		r, err := service.GetRequestWithClusters(request.ID)
@@ -101,12 +151,14 @@ func waitForClustersToStartProvisioning(service *cluster.ClusterService, request
 				return false, nil
 			}
 		}
+		req = *r
 		return true, nil
 	})
-	return err
+	return req, err
 }
 
-func waitForClustersToGetProvisioned(service *cluster.ClusterService, request cluster.Request) error {
+func waitForClustersToGetProvisioned(service *cluster.ClusterService, request cluster.Request) (cluster.RequestWithClusters, error) {
+	var req cluster.RequestWithClusters
 	err := wait.Poll(retryInterval, timeout, func() (done bool, err error) {
 		fmt.Println("Wait for clusters to get provisioned")
 		r, err := service.GetRequestWithClusters(request.ID)
@@ -132,9 +184,10 @@ func waitForClustersToGetProvisioned(service *cluster.ClusterService, request cl
 				return false, nil
 			}
 		}
+		req = *r
 		return true, nil
 	})
-	return err
+	return req, err
 }
 
 type MockConfig struct {
