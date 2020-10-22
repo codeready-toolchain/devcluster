3b76ac4bf84367026ba8a217469345b14f9fa74e
Michael Kleinhenz <michael@kleinhenz.net>
2020-10-22 18:42:47 +0200

    Turned generate around.

--- make/generate.mk
+++ make/generate.mk
@@ -1,5 +1,5 @@
 .PHONY: generate
 ## generates the asset bundle to be packaged with the binary
 generate:
-	go run -tags=dev pkg/static/assets_generate.go
 	cd ui && $(MAKE) build deploy
+	go run -tags=dev pkg/static/assets_generate.go
